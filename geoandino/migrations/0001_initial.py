526a72e60093172b150f698c7885617222d14bc7
Leandro Gomez <lgomez@devartis.com>
2017-07-10 12:40:56 -0300

    Agrego modelo SiteConfiguration
    refs:#2

---
+++ geoandino/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SiteConfiguration',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
+                ('title', models.CharField(max_length=255, verbose_name='title')),
+                ('description', models.TextField(null=True, verbose_name='description', blank=True)),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+        ),
+    ]
---
+++ geoandino/models.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from django_extensions.db import models
+
+class SiteConfiguration(models.TimeStampedModel, models.TitleDescriptionModel):
+    pass
