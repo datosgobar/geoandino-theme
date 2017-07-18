72dc326ab10ccca611f0a97e7cc8e8b3fb1af549
Leandro Gomez <lgomez@devartis.com>
2017-07-18 10:15:12 -0300

    Mejoras en el admin de SiteConfiguration
    refs:datosgobar/portal-geoandino#15

--- geoandino/admin.py
+++ geoandino/admin.py
@@ -4,6 +4,15 @@ from django.contrib import admin
 from geoandino.models import SiteConfiguration
 
 class SiteConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'default', )
+    list_display = ('title', 'default', 'publisher',)
+    fieldsets = (
+        ('Basic', {
+            'fields': ('title', 'description', 'default')
+        }),
+        ('Data Jsonar', {
+            'classes': ('collapse',),
+            'fields': ('publisher', ),
+        }),
+    )
 
 admin.site.register(SiteConfiguration, SiteConfigurationAdmin)
