964dc4dfb42562d3d976b2c583344455c0a8e00b
Julieta Colombo <jcolombo@devartis.com>
2017-07-20 15:11:13 -0300

    Agregadas configuraciones de urls y metada de redes sociales y descripcion de layers, documents y maps

--- geoandino/admin.py
+++ geoandino/admin.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
-
 from django.contrib import admin
-from geoandino.models import SiteConfiguration
+from geoandino.models import SiteConfiguration, FacebookAndGoogleMetadata, TwitterMetadata
 from django.utils.translation import ugettext as _
+
+
+class FacebookAndGoogleMetadataInline(admin.StackedInline):
+    model = FacebookAndGoogleMetadata
+    max_num = 1
+    verbose_name_plural = _('Social Media Metadata')
+    verbose_name = _('Facebook and Google')
+    fields = ['title', 'description', 'image']
+
+
+class TwitterMetadataInline(admin.StackedInline):
+    model = TwitterMetadata
+    max_num = 1
+    verbose_name_plural = _('Social Media Metadata')
+    verbose_name = _('Twitter')
+    fields = ['title', 'description', 'image']
 
 
 class SiteConfigurationAdmin(admin.ModelAdmin):
     list_display = ('title', 'default')
-
     fieldsets = [
-        (_('Landing'), {'fields': [_('title'), _('description'), _('default')]}),
-        (_('About (opcional)'), {'fields': [_('about_title'), _('about_description'), _('about_visible')]}),
+        (None, {'fields': [_('default')]}),
+        (_('Landing'), {'fields': [_('title'), _('description'), _('image_background')]}),
+        (_('Layers, documents and maps'), {'fields': [_('layer_description'), _('document_description'), _('map_description')]}),
+        (_('Contact'), {'fields': [_('facebook_url'), _('twitter_url'), _('github_url'), _('instagram_url'), _('youtube_url'), _('contact_mail')]}),
+        (_('About (optional)'), {'fields': [_('about_title'), _('about_description'), _('about_visible')]}),
     ]
+
+    inlines = [FacebookAndGoogleMetadataInline, TwitterMetadataInline]
 
 admin.site.register(SiteConfiguration, SiteConfigurationAdmin)
--- geoandino/models.py
+++ geoandino/models.py
@@ -5,10 +5,43 @@ from django.utils.translation import ugettext as _
 
 
 class SiteConfiguration(models_db.TimeStampedModel, models_db.TitleDescriptionModel):
-    default = models.BooleanField(default=False, )
+    default = models.BooleanField(default=False)
     about_visible = models.BooleanField(default=False, verbose_name=_('Visible'))
-    about_title = models.CharField(_('title'), max_length=255)
+    about_title = models.CharField(_('title'), max_length=255, default=None)
     about_description = models.TextField(_('description'), blank=True, null=True)
+    image_background = models.ImageField(null=True)
+    facebook_url = models.CharField(max_length=150, verbose_name=_('Facebook'), default="www.facebook.com/portal")
+    twitter_url = models.CharField(max_length=150, verbose_name=_('Twitter'), default="www.twitter.com/portal")
+    github_url = models.CharField(max_length=150, verbose_name=_('GitHub'), default="www.github.com/portal")
+    instagram_url = models.CharField(max_length=150, verbose_name=_('Instagram'), default="www.instagram.com/portal")
+    youtube_url = models.CharField(max_length=150, verbose_name=_('Youtube'), default="www.youtube.com/portal")
+    contact_mail = models.CharField(max_length=250, verbose_name=_('Contact Mail'), default="contact@portal.com")
+    layer_description = models.TextField(max_length=250, verbose_name=_('Layer Description'), blank=True, null=True)
+    map_description = models.TextField(max_length=250, verbose_name=_('Map Description'), blank=True, null=True)
+    document_description = models.TextField(max_length=250, verbose_name=_('Document Description'), blank=True, null=True)
 
     class Meta:
         ordering = ['created', ]
+
+
+class FacebookAndGoogleMetadata(models_db.TitleDescriptionModel):
+    site_configuration = models.ForeignKey(SiteConfiguration, on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+
+    @classmethod
+    def create(cls, site_configuration):
+        return cls(site_configuration=site_configuration)
+
+
+class TwitterMetadata(models_db.TitleDescriptionModel):
+    site_configuration = models.ForeignKey(SiteConfiguration, on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    tw_user = models.CharField(max_length=100, verbose_name=_("Twitter User"))
+
+    @classmethod
+    def create(cls, site_configuration):
+        return cls(site_configuration=site_configuration)
+
+
+
+
