29b69ae5c17b741b8493a17414f02ef151825b3d
Leandro Gomez <lgomez@devartis.com>
2017-07-21 12:12:48 -0300

    Agrego superTheme al admin de ResourceExtra
    refs:#datosgobar/postal-geoandino

--- geoandino/admin.py
+++ geoandino/admin.py
@@ -18,10 +18,13 @@ class SiteConfigurationAdmin(admin.ModelAdmin):
 
 class ResourceExtraAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified', 'resource', 'link_to_resource', 'resource_type', '__str__', )
-    list_display = ('__str__', 'resource_type','link_to_resource', 'created', 'modified', )
+    list_display = ('__str__', 'super_theme','resource_type','link_to_resource', 'created', 'modified', )
     fieldsets = (
         ('Basic', {
             'fields': ('created', 'modified', )
+        }),
+        ('data-json', {
+            'fields': ('super_theme', ),
         }),
         ('Readonly', {
             'fields': ('link_to_resource', 'resource_type', ),
