e593ec6b628421345b4c3794a4a286d5937a0709
Julieta Colombo <jcolombo@devartis.com>
2017-07-21 12:19:04 -0300

    Cuando se selecciona un SiteConfiguration como default, el resto se queda con el campo default en False (no puede haber más de un SiteConfiguration default)

--- geoandino/models.py
+++ geoandino/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import django.db.models as models
 from django_extensions.db import models as models_db
+from exclusivebooleanfield.fields import ExclusiveBooleanField
 from django.utils.translation import ugettext as _
 
 
 class SiteConfiguration(models_db.TimeStampedModel, models_db.TitleDescriptionModel):
-    default = models.BooleanField(default=False)
+    default = ExclusiveBooleanField(default=False)
     about_visible = models.BooleanField(default=False, verbose_name=_('Visible'))
     about_title = models.CharField(_('title'), max_length=255, default=None, blank=True, null=True)
     about_description = models.TextField(_('description'), blank=True, null=True)
