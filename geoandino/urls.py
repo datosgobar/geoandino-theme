4367d9a9e9e3d5c14df9c561b4e1bf2f28172a84
Leandro Gomez <lgomez@devartis.com>
2017-07-28 10:48:32 -0300

    Reemplazo la view del creacion de anuncios
    refs:#5

--- geoandino/urls.py
+++ geoandino/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 from geonode.urls import *
+from geoandino.overrides.urls import urlpatterns as override_urls
 
 urlpatterns = patterns('',
    url(r'^/?$',
        TemplateView.as_view(template_name='site_index.html'),
        name='home'),
- ) + urlpatterns
+ ) + override_urls + urlpatterns
