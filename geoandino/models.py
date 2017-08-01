d3a562848d3242561b2ba04e7ee12b8823ac5764
Julieta Colombo <jcolombo@devartis.com>
2017-08-01 12:00:05 -0300

    Agrego snippet de SuperThemeTaxonomy para templates

--- geoandino/models.py
+++ geoandino/models.py
@@ -8,6 +8,37 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 from geonode.base.models import TopicCategory
 
 
+AGRI = "agri"
+ECON = "econ"
+EDUC = "educ"
+ENER = "ener"
+ENVI = "envi"
+GOVE = "gove"
+HEAL = "heal"
+INTR = "intr"
+JUST = "just"
+REGI = "regi"
+SOCI = "soci"
+TECH = "tech"
+TRAN = "tran"
+
+# TODO: Use english texts and translated theme
+SUPER_THEME_CHOICES = (
+    (AGRI, "Agroganadería, pesca y forestación"),
+    (ECON, "Economía y finanzas"),
+    (EDUC, "Educación, cultura y deportes", ),
+    (ENER, "Energía"),    (ENVI, "Medio ambiente"),
+    (GOVE, "Gobierno y sector público"),
+    (HEAL, "Salud"),
+    (INTR, "Asuntos internacionales"),
+    (JUST, "Justicia, seguridad y legales"),
+    (REGI, "Regiones y ciudades"),
+    (SOCI, "Población y sociedad"),
+    (TECH, "Ciencia y tecnología"),
+    (TRAN, "Transporte"),
+)
+
+
 class TopicTaxonomy(models.Model):
     identifier = models.CharField(max_length=255)
     description = models.CharField(max_length=255, default=None, null=True, blank=True)
--- geoandino/utils/context_processors.py
+++ geoandino/utils/context_processors.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from geoandino.utils.conf import get_site_conf
-from geoandino.models import TopicTaxonomy
+from geoandino.models import TopicTaxonomy, SUPER_THEME_CHOICES
 
 
 def site_conf(request):
@@ -9,3 +9,7 @@ def site_conf(request):
 
 def taxonomies(request):
     return {"taxonomies": TopicTaxonomy.objects.all}
+
+
+def super_theme_taxonomies(request):
+    return {"super_theme_taxonomies": SUPER_THEME_CHOICES}
