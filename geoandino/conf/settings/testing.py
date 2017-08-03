a2526b54b53d313ce35000c8341d799b04be10b9
Leandro Gomez <lgomez@devartis.com>
2017-08-03 11:57:53 -0300

    Corro los scripts con sqlite

--- geoandino/conf/settings/testing.py
+++ geoandino/conf/settings/testing.py
@@ -4,15 +4,15 @@ from .base import *
 CATALOG_URL = "http://geonetwork:8080/"
 
 CATALOGUE['URL'] = '%sgeonetwork/srv/en/csw' % CATALOG_URL
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': ":memory:",
-#     },
-    # vector datastore for uploads
-#    'datastore' : {
-#        'ENGINE': 'django.db.backends.sqlite3',
-        #'ENGINE': '', # Empty ENGINE name disables
-#        'NAME': ":memory:",
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ":memory:",
+     },
+   # vector datastore for uploads
+    'datastore' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+       #'ENGINE': '', # Empty ENGINE name disables
+        'NAME': ":memory:",
+    }
+}
