41565f9177c06a9f2f3e136814f7d14615851198
Julieta Colombo <jcolombo@devartis.com>
2017-08-04 11:31:38 -0300

    Elimino el template index.html y unifico las urls overrideadas

--- geoandino/urls.py
+++ geoandino/urls.py
@@ -1,11 +1,4 @@
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-
 from geonode.urls import *
 from geoandino.overrides.urls import urlpatterns as override_urls
 
-urlpatterns = patterns('',
-   url(r'^/?$',
-       TemplateView.as_view(template_name='site_index.html'),
-       name='home'),
- ) + override_urls + urlpatterns
+urlpatterns = override_urls + urlpatterns
