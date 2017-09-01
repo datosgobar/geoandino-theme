2330bd7f53f4ee5df52ae4752d0b29bcde1be01a
julieta <julietanataliacolombo@yahoo.com.ar>
2017-09-01 13:12:32 -0300

    Si no hay datasets destacados, no muestro la sección

--- geoandino/utils/context_processors.py
+++ geoandino/utils/context_processors.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from geoandino.utils.conf import get_site_conf
 from geoandino.models import TopicTaxonomy, SUPER_THEME_CHOICES
+from geonode.base.models import ResourceBase
+from django.conf import settings
 
 
 def site_conf(request):
@@ -13,3 +15,10 @@ def taxonomies(request):
 
 def super_theme_taxonomies(request):
     return {"super_theme_taxonomies": SUPER_THEME_CHOICES}
+
+
+def featured(request):
+    queryset = ResourceBase.objects.filter(featured=True).order_by('-date')
+    if settings.RESOURCE_PUBLISHING:
+        queryset = queryset.filter(is_published=True)
+    return {"featured": queryset}
