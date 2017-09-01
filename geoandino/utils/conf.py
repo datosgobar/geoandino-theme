36bed3aec5b10966ade040e351966f535ca46dbb
julieta <julietanataliacolombo@yahoo.com.ar>
2017-09-01 15:39:28 -0300

    Agrego textos default

--- geoandino/utils/conf.py
+++ geoandino/utils/conf.py
@@ -6,8 +6,12 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 
 class NullSiteConfiguration:
     def __init__(self):
-        self.title = "Portal Geoandino"
-        self.description = "Portal Geoandino"
+        self.title = "Título del Portal"
+        self.description = "<b>Describí el portal.</b> Explicá de qué se trata tu catálogo de datos geoespaciales. " \
+                           "Por favor, hacelo en no más de tres líneas."
+        self.layer_description = "Estos son los conjuntos de datos espaciales y vectoriales disponibles."
+        self.document_description = ""
+        self.map_description = ""
         self.image_background_url = static('img/bg-jumbotron.jpg')
         self.logo_header_url = static('img/logo_ministerio.svg')
         self.logo_footer_url = static('img/argentinagob.svg')
