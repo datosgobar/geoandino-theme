a2c46a383a08601233c3c1a2917ee301105d7a79
Julieta Colombo <julietanataliacolombo@yahoo.com.ar>
2017-09-19 13:15:59 -0300

    Agrego field de anidación al form de grupos

--- geoandino/overrides/forms.py
+++ geoandino/overrides/forms.py
@@ -3,6 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 from announcements import forms as announcements_forms
 from account import forms as account_forms
+from geonode.groups import forms as group_forms
+from geonode.groups.models import GroupProfile
+
 
 class AnnouncementI18nForm(announcements_forms.AnnouncementForm):
     """
@@ -21,5 +24,29 @@ class AnnouncementI18nForm(announcements_forms.AnnouncementForm):
             'publish_end': _('publish end'),
         }
 
+
 class SignupCodeI18nForm(account_forms.SignupCodeForm):
     username = forms.CharField(max_length=30, required=False, label=_("Username"))
+
+
+def group_slugs():
+    groups = GroupProfile.objects.all()
+    return list(map(lambda g: g.slug, groups))
+
+
+def group_choices():
+    choices = []
+    for slug in group_slugs():
+        choices.append((slug, slug))
+    return choices
+
+
+def add_dependency_field(form):
+    form.base_fields['depends_on_group'] = forms.ChoiceField(label=_('Does it depend on another group?'),
+                                                             choices=group_choices(),
+                                                             initial=_("It doesn't depend on another group"),
+                                                             widget=forms.Select())
+
+
+add_dependency_field(group_forms.GroupForm)
+add_dependency_field(group_forms.GroupUpdateForm)
