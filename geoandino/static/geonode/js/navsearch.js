cdf2c33ba85321c5ba8a77ea54435183ef054aad
julieta <julietanataliacolombo@yahoo.com.ar>
2017-09-22 15:35:05 -0300

    Traigo js de navbar de la diseñadora

--- geoandino/static/geonode/js/navsearch.js
+++ geoandino/static/geonode/js/navsearch.js
@@ -1,3 +1,12 @@
 $('[data-navsearch]').on('click', 'a', function() {
-	$(this).parent('[data-navsearch]').toggleClass('open');
+
+	var container = $(this).parent('[data-navsearch]');
+
+	$(container).toggleClass('open');
+	$(container).find('form').toggle("slide", {direction:"right"}, 300);
+
+	if($(container).hasClass('open')){
+		$(container).find('input').val("");
+	}
+
 });
