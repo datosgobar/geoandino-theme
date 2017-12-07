53588feb647c2baf7eaff54d41c58ddc06bcb899
Christian Digiorno <cdigiorno@devartis.com>
2017-12-07 14:49:39 -0300

    Borrados archivos innecesarios

--- geoandino/bin/pycsw-admin.py
+++
@@ -1,4 +0,0 @@
-#!/home/cdigiorno/Documents/Geoandino/geoandino-theme/geoandino/bin/python
-# EASY-INSTALL-SCRIPT: 'pycsw==2.1.dev0','pycsw-admin.py'
-__requires__ = 'pycsw==2.1.dev0'
-__import__('pkg_resources').run_script('pycsw==2.1.dev0', 'pycsw-admin.py')
